f7c2eda7b5e7131980e1f5b8d0fa5b0898bb840f
VIDYASHANKARA <172364102+VIDYASHANKARA@users.noreply.github.com>
2025-01-28 10:21:48 +0530

    Update easy.java

--- Arrays/easy.java
+++ Arrays/easy.java
@@ -11,6 +11,7 @@ public class Main{
         System.out.println("The largest element is : "+largest);       
     }
 }
+//***********************************************************************************************
 //2]Second Largest Element in an Array without sorting
 public class Main{
     public static void main(String[] args){
@@ -29,6 +30,7 @@ public class Main{
         System.out.println("The second largest element is : "+ secondLargest);
     }
 }
+//***********************************************************************************************
 //3]Check if the array is sorted
 public class Main
 {
@@ -48,7 +50,7 @@ public class Main
 		}
 	}
 }
-
+//***********************************************************************************************
 //4]Remove duplicates from Sorted array
 public class Main{
     public static void main(String[] args){
@@ -75,32 +77,64 @@ public class Main{
         
     }
 }
+//***********************************************************************************************
 //5]Left Rotate an array by one place
 //6]Left rotate an array by D places
 public class Main
 {
 	public static void main(String[] args) {
-		int[] a={1,2,3,4,5,6,7,8};
-		int d=2;
+		int[] a={1,2,3,4,5,6,7,8,9,10};
+		int d=10;
 		int[] b=new int[a.length];
 		for(int i=d;i<a.length;i++){
 		    b[i-d]=a[i];
 		}
-		for(int i=a.length-d;i<a.length;i++){
-		    b[i]=a[d-i];
+		for(int i=0;i<d;i++){
+		    b[a.length-d+i]=a[i];
 		}
 		for(int i=0;i<b.length;i++){
 		    System.out.println(b[i]);
-		}
-		
+		}		
 	}
 }
-
-//7]
-//8]
-//9]
-//10]
-//11]
-//12]
-//13]
-//14]
+//***********************************************************************************************
+//7]Move Zeros to end
+public class Main{
+    public static void main(String[] args){
+        int[] a={1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
+        int[] b=new int[a.length];
+        int v=0;
+        for(int i=0;i<a.length;i++){
+            if(a[i]!=0){
+                b[v]=a[i];
+                v+=1;
+            }
+        }
+        for(int i=b.length-1;i<a.length-b.length;i++){
+            b[i]=0;
+        }
+        for(int i=0;i<b.length;i++){
+            System.out.println(b[i]);
+        }
+    }
+}
+//***********************************************************************************************
+//8]Linear Search
+public class Main{
+    public static void main(String[] args){
+        int[] a={1,2,34,5,67,434,2,6,3,4,6,84};
+        int search_element=2;
+        for(int i=0;i<a.length;i++){
+            if(a[i]==search_element){
+                System.out.println(i);
+            }
+        }
+    }
+}
+//***********************************************************************************************
+//9]Find the Union
+//10]Find missing number in an array
+//11]Maximum Consecutive Ones
+//12]Find the number that appears once, and other numbers twice.
+//13]Longest subarray with given sum K(positives)
+//14]Longest subarray with sum K (Positives + Negatives)
