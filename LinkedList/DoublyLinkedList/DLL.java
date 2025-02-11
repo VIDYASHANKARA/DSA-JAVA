ee7a35a8bb9f0f3ab1132eb44b4e5374dd41ccf1
VIDYASHANKARA <172364102+VIDYASHANKARA@users.noreply.github.com>
2025-02-11 21:53:38 +0530

    Create DLL.java

---
+++ LinkedList/DoublyLinkedList/DLL.java
@@ -0,0 +1,22 @@
+//1
+class Node{
+    int data;
+    Node prev;
+    Node next;
+
+    Node(int data1,Node prev1,Node next1){
+        this.data=data1;
+        this.prev=prev1;
+        this.next=next1;
+    }
+    Node(int data1){
+        this.data=data1;
+        this.prev=null;
+        this.next=null;
+    }
+}
+public class DLL{
+    public static void main(String[] args){
+
+    }
+}
